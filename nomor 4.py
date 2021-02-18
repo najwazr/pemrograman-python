d0f9759784458aa2bac663e3eea46263861d09f5
najwazr <najwazahraramadhani@gmail.com>
2021-02-18 13:32:08 +0700

    XI-5/26

---
+++ nomor 4.py
@@ -0,0 +1,10 @@
+vowels = 'aiueo'
+ip_str = 'Halo nama saya najwa zahra ramadhani, saya sedang belajar python'
+ip_str = ip_str.casefold()
+count = {}.fromkeys(vowels, 0)
+
+for char in ip_str:
+    if char in count:
+        count[char] += 1
+
+print(count)
